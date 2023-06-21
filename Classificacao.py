3db690f1300311fa87ab19b8a36d760dd10b7f3f
VictorVieiraV <vctin123@gmail.com>
2023-06-21 00:58:53 -0300

    Ajustes

--- Classificacao.py
+++ Classificacao.py
@@ -5,46 +5,62 @@ from scipy.spatial.distance import cdist
 from sklearn.datasets import make_moons
 from scipy.linalg import fractional_matrix_power
 
-n = 200
-n_labeled = 10
-alpha = 0.99
-sigma = 0.1
+# Configurações gerais
+numTotalPontos = 200  # Número total de pontos de dados
+numtPontosRotulados = 10  # Número de pontos de dados rotulados
+fatorSuavizacao = 0.99  # Fator de suavização
+sigmaCalculoDistancia = 0.1  # Parâmetro sigma para o cálculo da distância
 
-matrizX, matrizY = make_moons(n, shuffle=True, noise=0.1, random_state=None)
+# Geração dos dados
+matrizX, matrizY = make_moons(numTotalPontos, shuffle=True, noise=0.1, random_state=None)
 
-Y_input = np.concatenate(((matrizY[:n_labeled,None] == np.arange(2)).astype(float), np.zeros((n-n_labeled,2))))
+# Preparação dos rótulos de entrada
+rotolosEntrada = np.concatenate(((matrizY[:numtPontosRotulados,None] == np.arange(2)).astype(float), np.zeros((numTotalPontos-numtPontosRotulados,2))))
 
-dm = cdist(matrizX, matrizX, 'euclidean')
-rbf = lambda x, sigma: math.exp((-x)/(2*(math.pow(sigma,2))))
-vfunc = np.vectorize(rbf)
-W = vfunc(dm, sigma)
-np.fill_diagonal(W, 0)
+# Cálculo das distâncias euclidianas
+distanciaEuclidiana = cdist(matrizX, matrizX, 'euclidean')
 
-sum_lines = np.sum(W,axis=1)
-D = np.diag(sum_lines)
+# Função RBF (Função de Base Radial)
+baseRadial = lambda x, sigma: math.exp((-x)/(2*(math.pow(sigma,2))))
+vfunc = np.vectorize(baseRadial)
+W = vfunc(distanciaEuclidiana, sigmaCalculoDistancia)
+np.fill_diagonal(W, 0) # Zerando os elementos diagonais da matriz de similaridade
 
-D = fractional_matrix_power(D, -0.5)
-S = np.dot(np.dot(D,W), D)
+# Cálculo da soma das linhas da matriz de similaridade
+somaLinhasMatrizSimilaridade = np.sum(W,axis=1)
 
-n_iter = 400
+# Construção da matriz diagonal D
+matrizDiagonal = np.diag(somaLinhasMatrizSimilaridade)
 
-F = np.dot(S, Y_input)*alpha + (1-alpha)*Y_input
-for t in range(n_iter):
-    F = np.dot(S, F)*alpha + (1-alpha)*Y_input
+# Cálculo da matriz D^(-0.5)
+matrizDiagonal = fractional_matrix_power(matrizDiagonal, -0.5)
 
-Y_result = np.zeros_like(F)
-Y_result[np.arange(len(F)), F.argmax(1)] = 1
+# Construção da matriz de similaridade normalizada S
+matrizSimilaridadeNormalizada = np.dot(np.dot(matrizDiagonal,W), matrizDiagonal)
 
-Y_v = [1 if x == 0 else 0 for x in Y_result[0:,0]]
+# Configurações para iterações
+numIteracoes = 400
 
-color = ['red' if l == 0 else 'blue' for l in Y_v]
+# Propagação dos rótulos
+rotulosPropagados = np.dot(matrizSimilaridadeNormalizada, rotolosEntrada)*fatorSuavizacao + (1-fatorSuavizacao)*rotolosEntrada
+for t in range(numIteracoes):
+    rotulosPropagados = np.dot(matrizSimilaridadeNormalizada, rotulosPropagados)*fatorSuavizacao + (1-fatorSuavizacao)*rotolosEntrada
 
-print(f"\n")
+# Atribuição dos rótulos resultantes
+rotulosResultantes = np.zeros_like(rotulosPropagados)
+rotulosResultantes[np.arange(len(rotulosPropagados)), rotulosPropagados.argmax(1)] = 1
+
+# Preparação para visualização
+listaRotulosEmBinarios = [1 if x == 0 else 0 for x in rotulosResultantes[0:,0]] # Converter os rótulos para uma lista de valores binários (0 ou 1)
+color = ['red' if l == 0 else 'blue' for l in listaRotulosEmBinarios] # Atribuir cores com base nos rótulos
+
 print(f"\n")
 print(f'Sucesso!')
 
+# Imprimir dados originais
 plt.scatter(matrizX[0:,0], matrizX[0:,1])
 plt.show()
 
+# Imprimir dados com cores dos rótulos
 plt.scatter(matrizX[0:,0], matrizX[0:,1], color=color)
 plt.show()
